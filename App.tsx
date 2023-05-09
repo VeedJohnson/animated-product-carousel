c56c07ca3cc9f1247037e3f63cb8cf4b47117e8a
David Johnson <48daveed@gmail.com>
2023-05-09 15:05:39 +0100

    feat(app.tsx): add type definitions for components

--- App.tsx
+++ App.tsx
@@ -1,5 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+const { width, height } = Dimensions.get("window");
+const LOGO_WIDTH = 220;
+const LOGO_HEIGHT = 40;
+const DOT_SIZE = 40;
+const TICKER_HEIGHT = 40;
+const CIRCLE_SIZE = width * 0.6;
+
+type ItemProps = React.FC<{
+  imageUri: ImageSourcePropType;
+  heading: string;
+  description: string;
+  index: number;
+  scrollX: Animated.Value;
+}>;
+
+type TickerProps = React.FC<{
+  scrollX: Animated.Value;
+}>;
+
+type CircleProps = React.FC<{
+  scrollX: Animated.Value;
+}>;
+
+type PaginationProps = React.FC<{
+  scrollX: Animated.Value;
+}>;
 
 export default function App() {
   return (
